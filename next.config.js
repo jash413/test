0a0919b68caa913d259d59055d63d93317ffd1ef
Jash Mistry <99473238+jash413@users.noreply.github.com>
2024-11-26 09:03:17 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-    path: '/_next/image',
-    cacheDirectory: '/tmp/next-image-cache' // Use a writable path
+  experimental: {
+    serverComponentsExternalPackages: ['fs']
   }
 };
 
